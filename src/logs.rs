407422c60caa735e57ac110fbae4206e67d087cd
igrek51 <igrek51.dev@gmail.com>
2024-11-20 21:37:12 +0100

    chore: Use lazy static in global logs

--- src/logs.rs
+++ src/logs.rs
@@ -1,28 +1,19 @@
-use std::borrow::BorrowMut;
-use std::sync::{Mutex, MutexGuard, Once};
+use lazy_static::lazy_static;
+use std::sync::{Mutex, MutexGuard};
 use std::time::{SystemTime, UNIX_EPOCH};
 
-static mut LOGS_MESSAGES: Option<Mutex<Vec<String>>> = None;
-static INIT_LOGS: Once = Once::new();
+lazy_static! {
+    static ref LOGS_MESSAGES: Mutex<Vec<String>> = Mutex::new(vec![]);
+}
 
 fn global_logs_list<'a>() -> &'a Mutex<Vec<String>> {
-    INIT_LOGS.call_once(|| {
-        // Since this access is inside a call_once, before any other accesses, it is safe
-        unsafe {
-            *LOGS_MESSAGES.borrow_mut() = Some(Mutex::new(vec![]));
-        }
-    });
-    // As long as this function is the only place with access to the static variable,
-    // giving out a read-only borrow here is safe because it is guaranteed no more mutable
-    // references will exist at this point or in the future.
-    unsafe { LOGS_MESSAGES.as_ref().unwrap() }
+    &LOGS_MESSAGES
 }
 
 pub fn log(msg: &str) {
     let mut guard: MutexGuard<'_, Vec<String>> = global_logs_list().lock().unwrap();
-    let vector: &mut Vec<String> = &mut *guard;
     let time_str = current_time_str();
-    vector.push(format!("[{}] {}", time_str, msg));
+    guard.push(format!("[{}] {}", time_str, msg));
 }
 
 pub fn print_logs() {
